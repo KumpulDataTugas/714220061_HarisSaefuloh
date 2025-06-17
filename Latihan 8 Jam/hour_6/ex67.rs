36f28e2a1ca21c19e949502a9ccaa0a5f8cc5bfc
hrisz <harissaefuloh@gmail.com>
2025-06-17 22:23:18 +0700

    add: 8 hours rust practice

---
+++ Latihan 8 Jam/hour_6/ex67.rs
@@ -0,0 +1,15 @@
+mod sup_module{
+    fn a() -> i32 {
+        100
+    }
+    pub mod sub_module {
+        use super::a;
+        pub fn b() {
+            println!("{}",a());
+        }
+    }
+}
+
+fn main() {
+    sup_module::sub_module::b();
+}
